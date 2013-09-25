c7f7f58f7304bc7e5e91ec04e38b6231926bc9fa
Katie Soldau <ksoldau@gmail.com>
2013-09-25 16:49:00 -0400

    experiment - jQuery change element styles

---
+++ experiments/jquery/js/change_elements_style.js
@@ -0,0 +1,25 @@
+// to change the rectangle's margin 
+$(function() { // shorthand for document ready
+  $("#rectangle").css("margin-left", "200px");
+});
+// to change background colors
+$(function() {
+  $(".box").css("background-color", "#999");
+});
+// to change an object's font 
+$(function() {
+  $("#paragraph").css("font-family", "sans-serif");
+});
+// to change an object's font color
+$(function() {
+  $("#sentence").css("color", "#900");
+});
+// changing an element's width and height
+$(function() {
+  $("#rectangle2").css("width", "200px").css("height", "200px");
+});
+// changing the element's left position when it's absolutely positioned
+$(function() {
+  $("#green_box").css("left", "300px");
+});
+
